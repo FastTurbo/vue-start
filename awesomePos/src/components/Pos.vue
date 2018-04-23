7f7bbd9cb8df8f9d72432d262e9b30f7836507e5
FastTurbo <tpf1220@sina.com>
2018-04-23 14:46:38 +0800

    build awesome pos project,a vue.js demo
    Signed-off-by: tal <tianpengfei1>

---
+++ awesomePos/src/components/Pos.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="pos">
+      <p>
+        <i class="iconfont icon-home"></i>
+      </p>
+      Hello awesome pos!
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "pos"
+    }
+</script>
+
+<style scoped>
+
+</style>
